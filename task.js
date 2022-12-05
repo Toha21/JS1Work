94f5427888385a53c444f56a2b926c97a443dae9
Антон Гаврюхин <ah21@mail.ru>
2022-12-05 14:58:10 +0300

    task one

---
+++ task.js
@@ -0,0 +1,27 @@
+"use strict"
+function solveEquation(a, b, c) {
+  let arr = [];
+
+  const d = b ** 2 - 4 * a * c;
+   if ( d < 0 ) {
+    return arr;
+   }
+  if ( d === 0 ) {
+    arr.push( -b / ( 2 * a ));
+  } else {
+    arr.push((-b + Math.sqrt(d) ) / (2*a));
+    arr.push((-b - Math.sqrt(d) ) / (2*a));
+  }
+  
+  return arr;
+}
+
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+  
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
+
